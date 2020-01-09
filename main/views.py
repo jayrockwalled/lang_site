2e327918924297ba3af716ac7cd1cc3db412733f
jayrockwalled <35086981+jayrockwalled@users.noreply.github.com>
2020-01-08 23:09:54 -0800

    added tfidf back in

--- main/views.py
+++ main/views.py
@@ -60,22 +60,15 @@ def CommView(request,choice1,choice2,slug):
         error = 'Please only enter a single word.'
         return render(request, template_name='main/error.html', context={'error':error})
     
-    s3 = boto3.resource('s3',
-        aws_access_key_id='AKIAIQ77Y5D4LAGEMZLA',
-        aws_secret_access_key= 'Ndzg4MDUQXgLWPcOwWTUCPwb1TDBfW7063P')
-    # url_prefix = 's3://' + 'AKIAIQ77Y5D4LAGEMZLA' + ':' + 'uRJT/Ndzg4MDUQXgLWPcOwWTUCPwb1TDBfW7063P' + "@herokulangsite/"
-    url_prefix = 's3://herokulangsite'
-    w2v_url_1 = url_prefix+'/w2v/'+choice1+'_word2vec.model'
-    w2v_url_2 = url_prefix+'/w2v/'+choice2+'_word2vec.model'
-    tfidf_url_1 = url_prefix+'/tfidf/'+choice1+'_tfidf_df.csv'
-    tfidf_url_2 = url_prefix+'/tfidf/'+choice2+'_tfidf_df.csv'
-    # wv1 = choice1+'_word2vec.model'
-    # wv2 = choice2+'_word2vec.model'
+    # s3 = boto3.resource('s3',
+    #     aws_access_key_id='AKIAIQ77Y5D4LAGEMZLA',
+    #     aws_secret_access_key= 'Ndzg4MDUQXgLWPcOwWTUCPwb1TDBfW7063P')
+
     s3 = boto3.client('s3')
     w2v1 = s3.get_object(Bucket='herokulangsite', Key='w2v/'+choice1+'_word2vec.model')
     w2v2 = s3.get_object(Bucket='herokulangsite', Key='w2v/'+choice2+'_word2vec.model')
-    # tfidf1 = s3.get_object(Bucket='herokulangsite', Key='tfidf/'+choice1+'_tfidf_df.csv')
-    # tfidf2 = s3.get_object(Bucket='herokulangsite', Key='tfidf/'+choice2+'_tfidf_df.csv')
+    tfidf1 = s3.get_object(Bucket='herokulangsite', Key='tfidf/'+choice1+'_tfidf_df.csv')
+    tfidf2 = s3.get_object(Bucket='herokulangsite', Key='tfidf/'+choice2+'_tfidf_df.csv')
 
     try:
         # w2v_file1 = urlopen(w2v_url_1)
@@ -95,10 +88,10 @@ def CommView(request,choice1,choice2,slug):
         error = str(e)
         return render(request, template_name='main/error.html', context={'error':error})
 
-    # df1 = pd.read_csv(tfidf1['Body'])
-    # df2 = pd.read_csv(tfidf2['Body'])
-    # tfidf1 = df1[slug].to_string(index=False)
-    # tfidf2 = df2[slug].to_string(index=False)
+    df1 = pd.read_csv(tfidf1['Body'])
+    df2 = pd.read_csv(tfidf2['Body'])
+    tfidf1 = df1[slug].to_string(index=False)
+    tfidf2 = df2[slug].to_string(index=False)
     zipped = zip(most_similar1, most_similar2)
     context = {
         'zip': zipped, 
@@ -106,7 +99,7 @@ def CommView(request,choice1,choice2,slug):
         'freq2':freq2,
         'comm1':choice1,
         'comm2':choice2,
-        # 'tfidf1':tfidf1,
-        # 'tfidf2':tfidf2,
+        'tfidf1':tfidf1,
+        'tfidf2':tfidf2,
     }
     return render(request, template_name='main/results.html', context=context)
