26e6cd848f1894c2ad4d1614eff31767437ffef9
jayrockwalled <35086981+jayrockwalled@users.noreply.github.com>
2020-01-12 17:18:02 -0800

    turned off debug

--- lang_site/settings.py
+++ lang_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^nn2b(xsvxnnkjt^mhegix-aj5xpvjbu9b47*2$2b82m^^xj!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
